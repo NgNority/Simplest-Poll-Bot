0273bcda10865b8e953b430c08bb00c37e4b3b11
Daniel <danielruzin@gmail.com>
2020-06-03 20:02:23 -0400

    Update pollUpdates.js

--- commands/pollUpdates.js
+++ commands/pollUpdates.js
@@ -1,11 +1,11 @@
 module.exports = {
-	name: 'help',
-	description: 'help!',
+	name: 'pollUpdates',
+	description: 'pollUpdates!',
 	execute(message, args, Discord) {
                 message.delete();
         let embedHelp = new Discord.RichEmbed()
         .setTitle("Simplest Poll Usage")
-        .setDescription(' **Recent Updates:** \n \n **Hex Color Update** \n *This update adds the ability to use hex colors in the colors slot for even more colors! to use, insted of putting a color name there just do `-- [hex color]`. Note: Make sure you include that space in between the -- and hex color * \n \n**no-poll:**\n *This update adds the no-poll role, to use add a `no-poll` role to your server and put it on someone. You now see that they can\'t make polls*')
+        .setDescription(' **Recent Updates:** \n \n **Hex Color Update** \n This update adds the ability to use hex colors in the colors slot for even more colors! to use, insted of putting a color name there just do `-- [hex color]`. Note: Make sure you include that space in between the -- and hex color \n \n**no-poll:**\n This update adds the no-poll role, to use add a `no-poll` role to your server and put it on someone. You now see that they can\'t make polls')
         .setColor("#000000")
         .setFooter('Poll bot made by @NgNority#2187');
       
